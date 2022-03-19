c2f5c59a63dea580eac0b495c3c29b14b8cd1680
Mark Ajuwa <85886772+mikkybriggs@users.noreply.github.com>
2022-03-19 14:04:15 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+Metadata structure: {
+               name : // ERC721MyToken
+
+               image : // IPFS: https://ipfs.io/ipfs/QmVXZUUvwSed96P4asJw7YnfCZ6tjuwi3GZ2LeFZc5ptEq, 
+metadata.json : https://ipfs.io/ipfs/QmQusZBZ5D3NCL25e5CmoSQ3rZdeuPo4Pcwqngcj2z3SFL
+
+               description: // ERC721MyToken
+ 

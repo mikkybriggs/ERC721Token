b309a982014ad6e91a8fd9ab2040bd1e6e67d491
Mark Ajuwa <85886772+mikkybriggs@users.noreply.github.com>
2022-03-19 09:24:49 +0100

    Add files via upload

---
+++ ERC20MyToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract MyToken is ERC20, Ownable, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function buyToken(address receiver) public payable returns (uint256){
+        uint256 tokenAmount = msg.value / (1 * 10 ** decimals()) * 1000;
+
+        _mint(receiver, tokenAmount);
+
+        return tokenAmount;
+    }
+}
